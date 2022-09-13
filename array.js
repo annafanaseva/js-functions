307eaf12aee91fa1626c0ce7ef59b4e8e542e68c
Анна Афанасьева <ann.afanaseva777@gmail.com>
2022-09-13 20:04:20 +0300

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+let fruits = ["Яблоко", "Апельсин", "Слива", "Груша", "Банан", "Мандарин"];
+
+for (var i = 0; i < fruits.length; i++) {
+  console.log(i);
+  console.log(fruits[i]);
+}
+
+for (var i = fruits.length - 1; i > 0; i--) {
+  console.log(fruits[i]);
+  console.log(i);
+}
